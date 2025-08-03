75d292ea669dc227f8ccb291e65ca11ce4490cfd
Curtis Schlak <696163-realistschuckle@users.noreply.gitlab.com>
2025-08-03 14:23:30 -0500

    Fix the build?

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,13 @@
 module Example exposing (..)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Expect
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "This modlue"
+        [ test "Is it okay?" <|
+            \_ ->
+                Expect.equal True True
+        ]
